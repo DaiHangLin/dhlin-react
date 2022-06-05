79d363df3e51f833f072b692cb76acd371711393
dhlin <542072149@qq.com>
2022-06-05 13:45:27 +0800

    4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 set -e
+mkdir gh-pages-branch-1
 
 pwd
+mkdir gh-pages-branch-2
 remote=$(git config remote.origin.url)
+
+mkdir gh-pages-branch-3
 
 echo 'remote is: '$remote
 
@@ -11,16 +15,13 @@ mkdir gh-pages-branch
 
 cd gh-pages-branch
 
-git config --global user.email "$GH_EMAIL" >/dev/null 2>&1
+# git config --global user.email "$GH_EMAIL" >/dev/null 2>&1
 
-git config --global user.name "$GH_NAME" >/dev/null 2>&1
+# git config --global user.name "$GH_NAME" >/dev/null 2>&1
 
 git init
 
 git remote add --fetch origin "$remote"
-
-echo 'email is: '$GH_EMAIL
-echo 'name is: '$GH_NAME
 
 echo 'sitesource is: '$siteSource
 
@@ -35,14 +36,14 @@ cp -a "../${siteSource}/."
 
 ls -la
 
-git add -a
-git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+# git add -a
+# git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 
-git push --force --quiet origin ph-pages
+# git push --force --quiet origin ph-pages
 
-cd ..
+# cd ..
 
-rm -rf gh-pages-branch
+# rm -rf gh-pages-branch
 
 echo "Finished deployment!"
 
