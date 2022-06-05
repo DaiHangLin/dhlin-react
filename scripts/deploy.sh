c21245179f04ed2b8996c33cbba347b94f265f8d
dhlin <542072149@qq.com>
2022-06-05 13:23:30 +0800

    deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+pwd
+remote=$(git config remote.origin.url)
+
+echo 'remote is: '$remote
+
+mkdir gh-pages-branch
+
+cd gh-pages-branch
+
+git config --global user.email "$GH_EMAIL" >/dev/null 2>&1
+
+git config --global user.name "$GH_NAME" >/dev/null 2>&1
+
+git init
+
+git remote add --fetch origin "$remote"
+
+echo 'email is: '$GH_EMAIL
+echo 'name is: '$GH_NAME
+
+echo 'sitesource is: '$siteSource
+
+if git rev-parse --verify origin/gh-pages >/dev/null 2>&1; then
+    git checkout gh-pages
+    git rm -rf .
+else
+    git checkout --orphan gh-pages
+fi
+
+cp -a "../${siteSource}/."
+
+ls -la
+
+git add -a
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+
+git push --force --quiet origin ph-pages
+
+cd ..
+
+rm -rf gh-pages-branch
+
+echo "Finished deployment!"
+
